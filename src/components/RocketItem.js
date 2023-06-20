4b7028ff651a833d254efeb022b7a0574d321b90
unorji kingsley <unorjikingsley@gmail.com>
2023-06-20 18:08:35 +0100

    Styled reserved button

--- src/components/RocketItem.js
+++ src/components/RocketItem.js
@@ -21,19 +21,27 @@ const RocketItem = ({ rocket }) => {
   return (
     <li className="list-api">
       <div className="img-div">
-        <img
-          src={rocket.flickr_images[0]}
-          className="img-api"
-          alt="rocket"
-        />
+        <img src={rocket.flickr_images[0]} className="img-api" alt="rocket" />
       </div>
       <div className="div-api">
-        <span className="api-name">{rocket.rocket_name}</span>
+        <span>{rocket.rocket_name}</span>
         <p className="para-api">
-          {rocket.reserved === true && <span>Reserved</span>}
+          {rocket.reserved === true && (
+            <span className="api-name">Reserved</span>
+          )}
+          {' '}
           {rocket.description}
         </p>
-        <button onClick={(e) => { handleClick(e, rocket.id); }} className="reserved" type="button">{rocket.reserved === true ? 'Cancel Reservation' : 'Reserve Rocket'}</button>
+
+        <button
+          onClick={(e) => {
+            handleClick(e, rocket.id);
+          }}
+          className={`reserved ${rocket.reserved ? 'cancel' : 'reserve'}`}
+          type="button"
+        >
+          {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+        </button>
       </div>
     </li>
   );
