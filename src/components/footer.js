3234a25b713ee0911491308350c9c8679aa4aa0a
unorji kingsley <unorjikingsley@gmail.com>
2023-06-20 17:50:06 +0100

    Styled the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,15 @@
+import '../styles/footerStyle.css';
+
 const Footer = () => {
   const today = new Date();
   return (
-    <footer className="Footer">
+    <footer className="footer">
       <p className="foot-para">
         Copyright &copy;
         {' '}
         {today.getFullYear()}
         {' '}
-        Projects by Unorji Kingsley and Charles Andrews.
+        Projects by Unorji Kingsley and Charles Andrews
       </p>
     </footer>
   );
