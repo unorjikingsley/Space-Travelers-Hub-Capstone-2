24bc8da9babf16e1cf213fb0467caf25ef50c5c5
unorji kingsley <unorjikingsley@gmail.com>
2023-06-23 08:43:21 +0100

    displayed the reserved rocket in profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -5,18 +5,35 @@ function Profile() {
   const { missionList } = useSelector((state) => state.mission);
   const joinedMission = missionList.filter((mission) => mission.joined === true);
 
+  const { rockets } = useSelector((state) => state.rocket);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+
   return (
     <div>
-      <ul>
-        <h2>My Missions</h2>
-        {joinedMission.length > 0
-          ? joinedMission.map((mission) => (
-            <li key={mission.id}>
-              <h4>{mission.name}</h4>
-            </li>
-          ))
-          : <li>No Joined Missions Yet</li>}
-      </ul>
+      <div>
+        <ul>
+          <h2>My Missions</h2>
+          {joinedMission.length > 0
+            ? joinedMission.map((mission) => (
+              <li key={mission.id}>
+                <h4>{mission.name}</h4>
+              </li>
+            ))
+            : <li>No Joined Missions Yet</li>}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <h2>My Rockets</h2>
+          {reservedRockets.length > 0
+            ? reservedRockets.map((rocket) => (
+              <li key={rocket.id}>
+                <h3>{rocket.name}</h3>
+              </li>
+            ))
+            : <li>No reserved rockets</li>}
+        </ul>
+      </div>
     </div>
   );
 }
